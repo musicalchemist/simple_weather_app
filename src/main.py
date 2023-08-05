71c504ab65cbecd3c23e52c712dbcad3bdd55161
Dave Berry <berrydp@gmail.com>
2023-08-05 16:25:37 -0700

    fixed styling for weather and forecast results

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 from weather import Weather
 
 weather = Weather()
@@ -13,36 +14,44 @@ def main():
         weather_data = weather.fetch_weather(city_name)
         if weather_data:
             st.header(f"Current Weather in {city_name}:")
-            st.markdown(f"**{weather_data['weather'][0]['description']}**")
-            st.markdown(f"**Temperature:** {weather_data['main']['temp']}°C")
+
+            # Card-like layout for weather data
+            col1, col2 = st.columns(2)
+            with col1:
+                st.markdown(f"<div style='background-color: #f5f5f5; padding: 10px; border-radius: 10px;'>"
+                            f"<h3 style='color: black;'>Weather</h3>"
+                            f"<h2 style='color: black;'>{weather_data['weather'][0]['description'].upper()}</h2></div>", unsafe_allow_html=True)
+            with col2:
+                st.markdown(f"<div style='background-color: #f5f5f5; padding: 10px; border-radius: 10px;'>"
+                            f"<h3 style='color: black;'>Temperature</h3>"
+                            f"<h2 style='color: black;'>{weather_data['main']['temp']}°C</h2></div>", unsafe_allow_html=True)
             
             forecast_data = weather.fetch_forecast(city_name)
             if forecast_data:
                 st.subheader("3-Day Forecast:")
-                dates = [forecast['dt_txt'] for forecast in forecast_data]
+                # Parse date string to only include date (not time)
+                dates = [datetime.datetime.strptime(forecast['dt_txt'], '%Y-%m-%d %H:%M:%S').date() for forecast in forecast_data]
                 temps = [forecast['main']['temp'] for forecast in forecast_data]
                 descriptions = [forecast['weather'][0]['description'] for forecast in forecast_data]
-
-                for date, desc, temp in zip(dates, descriptions, temps):
-                    st.markdown(f"**Date:** {date}")
-                    st.markdown(f"**Weather:** {desc}")
-                    st.markdown(f"**Temperature:** {temp}°C")
 
                 # Create a DataFrame for the forecast data
                 forecast_df = pd.DataFrame({
                     'Date': dates,
-                    'Temperature': temps
+                    'Weather': descriptions,
+                    'Temperature (°C)': temps
                 })
+
+                # Display forecast data as a table
+                st.table(forecast_df)
                 
-                # Convert the 'Date' column to datetime for plotting
-                forecast_df['Date'] = pd.to_datetime(forecast_df['Date'])
-                
-                # Create a line plot of the forecast temperatures
+                # Plotting forecast data
                 fig, ax = plt.subplots()
                 ax.plot(forecast_df['Date'], forecast_df['Temperature'], marker='o')
                 ax.set_xlabel('Date')
                 ax.set_ylabel('Temperature (°C)')
                 ax.set_title('3-Day Temperature Forecast')
+                plt.xticks(rotation=45)
+                ax.xaxis.set_major_locator(plt.MaxNLocator(3))  # Reduce number of x-axis ticks
                 st.pyplot(fig)
 
             # Log search history...
@@ -51,3 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
